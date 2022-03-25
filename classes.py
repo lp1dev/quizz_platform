1a2feff3850dc5238e7aad6e7aa79c76fd3adc02
Jeremie <lp1@xps13.lan>
2022-03-25 14:22:57 +0100

    first fully working version

--- classes.py
+++ classes.py
@@ -5,7 +5,7 @@ class Question:
         return
 
     def add_answer(self, answer, is_right):
-        self.answers.append({answer:is_right})
+        self.answers.append({"title":answer, "is_right":is_right})
     
     def __str__(self):
         return self.title + "%s" %self.answers
@@ -19,7 +19,14 @@ class Quizz:
 
     def parse(self, content):
         q = None
+        commented = False
         for line in content:
+            if "<!--" in line:
+                commented = True
+            if "-->" in line:
+                commented = False
+            if commented:
+                break
             if line.strip().startswith('### '):
                 if q and q not in self.questions:
                     self.questions.append(q)
@@ -27,10 +34,20 @@ class Quizz:
             elif line.strip().startswith('# '):
                 self.title = line.strip()[2:]
             elif q and line.strip().startswith('- '):
-                q.add_answer(line.strip()[2:], "#true" in line)
+                q.add_answer(line.strip()[2:].replace('#true', ''), "#true" in line)
             if q and q not in self.questions:
                 self.questions.append(q)
         return
 
     def __str__(self):
         return self.title + "%s" %self.questions
+
+    def html(self):
+        output = f"""<h1>{self.title}</h1>
+<form method="POST"><h3>0 - Indiquez votre NOM + Prénom (utilisés pour la notation)</h3><div><input type="text" name="name" id="name"/></div>"""
+        for question in self.questions:
+            output += f"""<h3>{question.title}</h3>"""
+            for count, answer in enumerate(question.answers):
+                output +=  f"""<div><input type="checkbox" id="{count}" name="{answer['title']}"><label>{answer['title']}</label></div>"""
+        output += "<br/><div><input type='submit' value='Valider mes réponses'/></div></form>"
+        return output
--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, request
 from os import listdir
 from classes import Quizz
+from datetime import datetime
 
 app = Flask(__name__)
 quizzes = []
@@ -11,10 +12,39 @@ quizzes = []
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route("/quizz/<quizz_id>", methods=["GET"])
+def quizz_view(quizz_id):
+    return quizzes[int(quizz_id)].html()
+
+@app.route("/quizz/<quizz_id>", methods=["POST"])
+def quizz_post(quizz_id):
+    name = request.form.get('name')
+    if not name:
+        return "Invalid Name"
+    dt = datetime.now().strftime("%d_%m_%Y-%H:%M:%S")
+    output_f = "%s-%s.txt" %(name, dt)
+    
+    points = 0
+    
+    with open(output_f, "a+") as f:
+        f.write('Student : %s\n' %name)
+        for question in quizzes[int(quizz_id)].questions:
+            f.write('----\n\n'+question.title+'\n')
+            valid = True
+            for answ_id, answer in enumerate(question.answers):
+                if answer.get('is_right') and request.form.get(answer.get('title')) != 'on':
+                    valid = False
+                if not answer.get('is_right') and request.form.get(answer.get('title')) == 'on':
+                    valid = False
+                f.write(' - '+answer.get('title')+ ('[OK]' if valid else '[KO]') + '\n')
+            if valid:
+                points += 1
+        f.write('\nGrade : '+str(points)+' / '+ str(len(quizzes[int(quizz_id)].questions)) + '\n')
+    return "Vos réponses ont bien été prises en compte!"
+
 if __name__ == "__main__":
     for f in listdir():
         if f.endswith('.md'):
             q = Quizz(filename=f)
             quizzes.append(q)
-    print(quizzes[0])
     app.run(threaded=True)
